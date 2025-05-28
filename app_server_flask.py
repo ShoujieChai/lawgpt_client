f6bf4b5ec53659cd7fbc2b0f3926c14dac07f781
JackChai <jackchai@jackchais-air.mynetworksettings.com>
2025-05-28 01:31:43 -0700

    update

--- app_server_flask.py
+++ app_server_flask.py
@@ -131,7 +131,7 @@ def query_endpoint():
 #     return jsonify({"response": response})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
 
 # if __name__ == "__main__":
